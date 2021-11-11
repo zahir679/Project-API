d77ff8a30c74b384928e98b610cf319f7db440c7
helboi4 <h.hopecarter@gmail.com>
2021-11-11 15:38:51 +0000

    Filled in all SongService methods using a SongDAO we have created. Added comments to makee SongController readable for the frontend team

--- src/main/java/com/bluechickenfm/song/SongController.java
+++ src/main/java/com/bluechickenfm/song/SongController.java
@@ -14,85 +14,101 @@ public class SongController {
     }
 
     //GET
+    //Method to get all songs
     @GetMapping("/songs")
     public @ResponseBody List<Song> getAllSongs(){
         return songService.getAllSongs();
     }
 
+    //Method to get a single song by its id (primary key)
     @GetMapping("/songs/{id}")
     public @ResponseBody Song getSongById(@PathVariable int id){
         return songService.getSongById(id);
     }
 
+    //Method to get songs by artist_id
     @GetMapping("/songs/{artist_id}")
     public @ResponseBody List<Song> getSongsByArtist(@PathVariable int artist_id){
         return songService.getSongsByArtist(artist_id);
     }
 
+    //Method to get songs by album_id
     @GetMapping("/songs/{album_id}")
     public @ResponseBody List<Song> getSongsByAlbum(@PathVariable int album_id){
         return songService.getSongsByAlbum(album_id);
     }
 
+    //Method to get songs by genre
     @GetMapping("/songs/{genre}")
     public @ResponseBody List<Song> getSongsByGenre(@PathVariable String genre){
         return songService.getSongsByGenre(genre);
     }
 
+    //Method to get songs by year
     @GetMapping("/songs/{release_year}")
     public @ResponseBody List<Song> getSongsByYear(@PathVariable int release_year){
         return songService.getSongsByYear(release_year);
     }
 
+    //Method to get songs by decade
     @GetMapping("/songs/{release_decade}")
     public @ResponseBody List<Song> getSongsByDecade(@PathVariable int release_decade){
         return songService.getSongsByDecade(release_decade);
     }
 
     //POST
+    //Add song
     @PostMapping()
     public void addSong(@RequestBody Song song) {
         songService.addSong(song);
     }
 
     //PUT
+    //Method to update a whole song
     @PutMapping("/songs/{id}")
     public void updateSong(@PathVariable int id,
                            @RequestBody Song song) {
         songService.updateSong(id, song);
     }
 
+    //Method to update a given song's name
     @PutMapping("/songs/{id}")
     public void updateSongName(@PathVariable int id, String name) {
         songService.updateSongName(id, name);
     }
 
+    //Method to update a given song's genre
     @PutMapping("/songs/{id}")
     public void updateSongGenre(@PathVariable int id, String genre) {
         songService.updateSongGenre(id, genre);
     }
 
+    //Method to update a given song's artist_id
     @PutMapping("/songs/{id}")
     public void updateSongArtistId(@PathVariable int id, int artist_id) {
         songService.updateSongArtistId(id, artist_id);
     }
 
+    //Method to update a given song's album_id
     @PutMapping("/songs/{id}")
     public void updateSongAlbumId(@PathVariable int id, String album_id) {
         songService.updateSongAlbumId(id, album_id);
     }
 
+    //Method to update a given song's release date
     @PutMapping("/songs/{id}")
     public void updateSongReleaseDate(@PathVariable int id, LocalDate release_date) {
         songService.updateSongReleaseDate(id, release_date);
     }
 
+    //Method to update a given song's language
     @PutMapping("/songs/{id}")
     public void updateSongLanguage(@PathVariable int id, String language) {
         songService.updateSongLanguage(id, language);
     }
 
     //DELETE
+    //Method to delete a song
     @DeleteMapping("/songs/{id}")
     public void deleteSong(@PathVariable int id) {
         songService.deleteSong(id);
---
+++ src/main/java/com/bluechickenfm/song/SongDAO.java
@@ -0,0 +1,39 @@
+package com.bluechickenfm.song;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface SongDAO {
+
+    List<Song> getAllSongs();
+
+    Song getSongById(int id)
+
+    List<Song> getSongsByArtist(int artist_id);
+
+    List<Song> getSongsByAlbum(int release_year);
+
+    List<Song> getSongsByGenre(String genre);
+
+    List<Song> getSongsByDecade(int release_decade);
+
+    void addSong(Song song);
+
+    void updateSong(Song song);
+
+    void updateSongName(int id, String name);
+
+    void updateSongGenre();
+
+    void updateSongDuration();
+
+    void updateSongLanguage();
+
+    void updateSongArtistId();
+
+    void updateSongAlbumId();
+
+    void deleteSong();
+
+    void updateSongReleaseDate(int id, LocalDate release_date);
+}
