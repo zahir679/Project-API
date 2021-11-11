f840f3fa491e84446c0d6afccc806103c1dbce85
zahir679 <zahirahmed0512@gmail.com>
2021-11-11 14:57:41 +0000

    Set up 3 tables and connecting it to the "chicken" database.

---
+++ src/main/resources/db.migration/V1__Initial_Setup.sql
@@ -0,0 +1,27 @@
+CREATE TABLE artists(
+  id BIGSERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  nationality TEXT NOT NULL,
+  biggest_hit TEXT NOT NULL,
+);
+
+CREATE TABLE albums(
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    artist_id INT REFERENCES artists(id),
+    genre TEXT NOT NULL,
+    release_date DATE NOT NULL,
+    number_of_tracks INT NOT NULL
+);
+
+CREATE TABLE songs (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    genre TEXT NOT NULL,
+    duration INT NOT NULL,
+    artist_id INT REFERENCES artists(id),
+    album_id INT REFERENCES albums(id),
+    release_date DATE NOT NULL,
+    languages TEXT NOT NULL,
+    platform TEXT NOT NULL
+);
