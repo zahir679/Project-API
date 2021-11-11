8861cef11a545e5d6a2fb1b1347ea2ce7158bc16
queenieyear@gmail.com <you@example.com>
2021-11-11 14:30:16 +0000

    Added PUT, POST, DELETE methods in SongController.java

--- src/main/java/com/bluechickenfm/song/SongService.java
+++ src/main/java/com/bluechickenfm/song/SongService.java
@@ -1,5 +1,6 @@
 package com.bluechickenfm.song;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class SongService {
@@ -7,21 +8,51 @@ public class SongService {
 
     }
 
-    public Song getSongById() {
+    public Song getSongById(int id) {
     }
 
-    public List<Song> getSongsByArtist() {
+    public List<Song> getSongsByArtist(int artist_id) {
     }
 
-    public List<Song> getSongsByAlbum() {
+    public List<Song> getSongsByAlbum(int album_id) {
     }
 
-    public List<Song> getSongsByGenre() {
+    public List<Song> getSongsByGenre(String genre) {
     }
 
-    public List<Song> getSongsByYear() {
+    public List<Song> getSongsByYear(int release_year) {
     }
 
-    public List<Song> getSongsByDecade() {
+    public List<Song> getSongsByDecade(int release_decade) {
+    }
+
+    public void addSong(Song song) {
+    }
+
+    public void updateSong(int id, Song song) {
+    }
+
+    public void updateSongName(int id, String name) {
+    }
+
+    public void updateSongGenre(int id, String genre) {
+    }
+
+    public void updateSongDuration(int id, int duration) {
+    }
+
+    public void updateSongReleaseDate(int id, LocalDate release_date) {
+    }
+
+    public void updateSongLanguage(int id, String language) {
+    }
+
+    public void updateSongArtistId(int id, int artist_id) {
+    }
+
+    public void updateSongAlbumId(int id, String album_id) {
+    }
+
+    public void deleteSong(int id) {
     }
 }
