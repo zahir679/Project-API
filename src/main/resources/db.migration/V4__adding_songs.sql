3085b37ec55e6b7e6c5a550752ec8774ce52013e
zahir679 <zahirahmed0512@gmail.com>
2021-11-18 09:34:14 +0000

    V4 sql file for adding songs

---
+++ src/main/resources/db.migration/V4__adding_songs.sql
@@ -0,0 +1,118 @@
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('My Luv', 'K-pop', 180, 118, 118,  '2018-09-15',  'Korean', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Ideal', 'K-pop', 210, 118, 118,  '2018-10-15', 'Korean', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Hello Tutorial', 'K-pop', 250, 118, 118, '2018-10-15', 'Korean', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Juicy', 'Rap', 180 ,75 ,75,  '1994-08-09', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Big Poppa', 'Rap', 192, 75 ,75,  '1994-12-24', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration,  artist_id, album_id, release_date, languages, platform) VALUES ('One More Chance', 'Rap', 188 , 75 ,75,  '1995-06-06', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Hells Bells', 'Rock',  310, 3,  3, '1980-08-31', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Rock and Roll Aint Noise Pollution', 'Rock',  310, 3,  3, '1981-03-15', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Shoot To Thrill', 'Rock',  255, 3,  3, '1980-07-25', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Hello', 'Soul', 295 ,4 , 4, '2015-10-23', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('When We Were Young', 'Soul', 291 ,4 , 4, '2016-01-22', 'English', 'Spotify');
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Remedy', 'Soul', 245 ,4 , 4, '2015-11-20', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('River Lea', 'Soul', 225 ,4 , 4, '2015-11-20', 'English', 'Spotify');
+
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Re-Rewind', 'UK Garage', 242 , 8 , 8, '1999-09-27', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Woman Trouble', 'UK Garage', 240 , 8 , 8, '2000-06-08', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Movin Too Fast', 'UK Garage', 234 , 8 , 8, '1999-07-26', 'English', 'Spotify');
+
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Three Little Birds', 'Reggae',  180, 13,  13, '1977-09-12', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Jammin', 'Reggae',  311, 13,  13, '1977-06-03', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('One Love', 'Reggae',  172, 13,  13, '1977-04-16', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Lets Dance', 'Pop', 457 ,22 , 22, '1983-03-14', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('China Girl', 'Pop', 332 ,22 , 22, '1983-05-31', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Modern Love', 'Pop', 286 ,22 , 22, '1983-09-12', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Thinking Out Loud', 'Pop', 281 , 29 , 29, '2014-09-30', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Photograph', 'Pop', 259 , 29 , 29, '2015-05-11', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Sing', 'Pop', 235 , 29 , 29, '2014-04-07', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('The Real Slim Shady', 'Rap', 284 , 32 , 32, '2000-04-15', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Stan', 'Rap', 404 , 32 , 32, '2000-11-21', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Marshall Mathers', 'Rap', 320 , 32 , 32, '2000-05-23', 'English', 'Spotify');
+
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Pyramids', 'R&B', 592 , 36 , 36, '2012-06-08', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Thinkin Bout You', 'R&B', 200 , 36 , 36, '2012-04-17', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Sweet Life', 'R&B', 266 , 36 , 36, '2012-07-06', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Strangers In The Night', 'Jazz', 155 , 37 , 37, '1966-01-26', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Summer Wind', 'Jazz', 173 , 37 , 37, '1966-01-26', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Call Me', 'Jazz', 187 , 37 , 37, '1966-01-26', 'English', 'Spotify');
+
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Take Me To Church', 'Soul', 242 , 46 , 46, '2014-09-13', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Sedated', 'Soul', 207 , 46 , 46, '2014-03-09', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('From Eden', 'Soul', 283 , 46 , 46, '2014-09-13', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Mi Gente', 'Reggaeton', 185 , 54 , 54, '2017-06-30', 'Spanish', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Ahora', 'Reggaeton', 255 , 54 , 54, '2018-02-26', 'Spanish', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Machika', 'Reggaeton', 190 , 54 , 54, '2018-01-09', 'Spanish', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Mr.Launcher', 'J-Pop', 249 , 63 , 63, '2015-03-04', 'Japanese', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Rising Hope', 'J-Pop', 253 , 63 , 63, '2015-03-04', 'Japanese', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('ANTIHERO', 'J-Pop', 202 , 63 , 63, '2015-03-04', 'Japanese', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Yukitoki', 'J-Pop', 269 , 77, 77, '2013-04-17', 'Japanese', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Laterality', 'J-Pop', 257 , 77, 77, '2012-11-09', 'Japanese', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Vidro Moyou', 'J-Pop', 291 , 77, 77, '2012-02-29', 'Japanese', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Stand By Me', 'Soul', 165 , 79, 79, '1964-03-23', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Hey Hey Baby', 'Soul', 135 , 79, 79, '1964-03-23', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('I Need Your Lovin', 'Soul', 167 , 79, 79, '1964-03-23', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Bombastic', 'Reggae', 247 , 99, 99, '2015-06-05', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Gal Yu a Pepper', 'Reggae', 258 , 99, 99, '2015-07-011', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('It Wasnt Me Remix', 'Reggae', 223 , 99, 99, '2015-07-11', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Black Swan', 'K-Pop', 198 , 111, 111, '2020-01-17', 'Korean', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('On', 'K-Pop', 246 , 111, 111, '2020-02-21', 'Korean', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Dionysus', 'K-Pop', 248 , 111, 111, '2020-02-21', 'Korean', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('My Love', 'Pop', 233 , 114, 114, '2000-10-30', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('Against All Odds', 'Pop', 202 , 114, 114, '2000-09-15', 'English', 'Spotify');
+
+INSERT INTO songs (song_name, genre, duration, artist_id, album_id, release_date, languages, platform) VALUES ('What Makes A Man', 'Pop', 231 , 114, 114, '2000-12-18', 'English', 'Spotify');
